379e41021d63d4b02b6537d195d17a020b1696e2
alexon <alexon.vanes.ifpinhais@gmail.com>
2023-12-01 09:15:33 -0300

    storage contatos com tela de signin e signup

--- contatos-TA/src/app/view/usuarios/signup/signup.page.ts
+++ contatos-TA/src/app/view/usuarios/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  formCadastrar : FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private presentAlert: AlertService) {
+    this.formCadastrar = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl(''),
+      confSenha: new FormControl('')
+    })
+   }
 
-  ngOnInit() {
+   ngOnInit() {
+    this.formCadastrar = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confSenha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  submitForm() :boolean{
+    if(!this.formCadastrar.valid){
+      this.presentAlert.presentAlert("Erro", "Erro ao preencher o forumulario")
+      return false;
+    }else{
+      this.cadastrar();
+      return true
+    }
+  }
+
+  private cadastrar(){
+    this.presentAlert.presentAlert("Olá", "Cadastro realizado com sucesso")
+    this.router.navigate(["signin"]);
+  }
+
+  irParaSignIn(){
+    this.router.navigate(["signin"])
   }
 
 }
